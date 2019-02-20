11927970dff925e387b26726df0ba8875b2701e1
mzaini30 <muhzaini30@gmail.com>
2019-02-20 17:41:29 +0800

    oke

--- app/src/main/assets/app/app.js
+++ app/src/main/assets/app/app.js
@@ -1,55 +1,120 @@
+$('.navbar-nav a').click(function(){
+	$('.navbar-toggle').click()
+})
+
+ke_atas = function(){
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700)
+}
+
 update_persen = function(){
-	sudah_selesai = $('.cek-oke').length
-	semua = $('.cek').length
+	data = JSON.parse(localStorage.getItem('jumlah-halaman')).map(Number)
+	jumlah = 0
+	for (n in data){
+		jumlah += data[n]
+	}
+	sudah_selesai = jumlah // total jumlah halaman semuanya
+	semua = Number(localStorage.getItem('target-halaman')) // target halaman
 	if (semua > 0){
 		persen_selesai = Math.round(sudah_selesai / semua * 100)	
 	} else {
 		persen_selesai = 0
 	}
-	$('.progress-bar').attr('aria-valuenow', persen_selesai).css('width', persen_selesai + '%')
-	$('.alert-success').html(persen_selesai + '% Complete')
+	$('.progress-jumlah-halaman').attr('aria-valuenow', persen_selesai).css('width', persen_selesai + '%')
 }
-$('.navbar-nav li a').click(function(){
-	$('.navbar-toggle').click()
-})
 
-if (localStorage.getItem('reading challenge') == null){
-	$('body').addClass('atur-tinggi')
-	$('#myModal').modal()
-	$('.progress').hide()
-} else {
-	$('body').removeClass('atur-tinggi')
-	$('.isi-target').html(localStorage.getItem('reading challenge'))
-	update_persen()
+grafik = function(){
+	data_grafik = JSON.parse(localStorage.getItem('jumlah-halaman')).reverse()
+	var ctx = document.getElementById("myChart").getContext('2d');
+	var myChart = new Chart(ctx, {
+	    type: 'bar',
+	    data: {
+	        labels: ["", "", "", "", "", ""],
+	        datasets: [{
+	            label: 'Jumlah Halaman',
+	            // data: [12, 19, 3, 5, 2, 3],
+	            data: [
+	            	data_grafik[5],
+	            	data_grafik[4],
+	            	data_grafik[3],
+	            	data_grafik[2],
+	            	data_grafik[1],
+	            	data_grafik[0]
+	            ],
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)',
+	                'rgba(255, 159, 64, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255,99,132,1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true
+	                }
+	            }]
+	        }
+	    }
+	});
+
+	if (data_grafik[0] != null){
+		$('.angka').html(Math.max(...data_grafik.map(Number)))
+	}
 }
-$('.buka-modal').click(function(){
-	$('body').addClass('atur-tinggi')
-	$('#myModal').modal()
+
+if (localStorage.getItem('jumlah-halaman') === null){
+	localStorage.setItem('jumlah-halaman', '[]')
+}
+
+$('.nama').val(localStorage.getItem('nama'))
+$('.judul-buku').val(localStorage.getItem('judul-buku'))
+$('.genre').val(localStorage.getItem('genre'))
+$('.premis').val(localStorage.getItem('premis'))
+$('.target-halaman').val(localStorage.getItem('target-halaman'))
+grafik()
+update_persen()
+
+rand = quote[Math.floor(Math.random() * quote.length)]
+$('.quote-isi').html(rand[0])
+$('.quote-author').html(rand[1])
+
+$('.kirim').click(function(){
+	localStorage.setItem('nama', $('.nama').val())
+	localStorage.setItem('judul-buku', $('.judul-buku').val())
+	localStorage.setItem('genre', $('.genre').val())
+	localStorage.setItem('premis', $('.premis').val())
+	localStorage.setItem('target-halaman', $('.target-halaman').val())
+	data_jumlah_halaman = JSON.parse(localStorage.getItem('jumlah-halaman'))
+	data_jumlah_halaman.push($('.jumlah-halaman').val())
+	localStorage.setItem('jumlah-halaman', JSON.stringify(data_jumlah_halaman))
+	ke_atas()
+	grafik()
+	update_persen()
+	// location.reload()
+	kirim_nama = $('.nama').val().replace(/ /g, '+')
+	kirim_judul_buku = $('.judul-buku').val().replace(/ /g, '+')
+	kirim_genre = $('.genre').val().replace(/ /g, '+')
+	kirim_premis = $('.premis').val().replace(/ /g, '+')
+	kirim_jumlah_halaman = $('.jumlah-halaman').val().replace(/ /g, '+')
+	// location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSeybQuZ_-aldTdw3FwwhAIuNc_rj9kGn1V0cOTwSMhwyFbeRg/viewform?usp=pp_url&entry.1254118809=' + kirim_nama + '&entry.1071710605=' + kirim_judul_buku + '&entry.1653769640=' + kirim_genre + '&entry.46621118=' + kirim_premis + '&entry.816932494=' + kirim_jumlah_halaman
+	location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSefJjGDYt8FGF8VtmReIMHYPSEAJ-9qD2i_Zbeyy42qjj_MPg/viewform?usp=pp_url&entry.1254118809=' + kirim_nama + '&entry.1071710605=' + kirim_judul_buku + '&entry.1653769640=' + kirim_genre + '&entry.46621118=' + kirim_premis + '&entry.816932494=' + kirim_jumlah_halaman
 })
 
-$('.count').click(function(){
-	$('body').removeClass('atur-tinggi')
-	hasil_bagi = Math.ceil($('.total-pages').val() / $('.target-finish').val())
-	// $('title').html(hasil_bagi)
-	list_halaman = []
-	for (x = hasil_bagi; x <= $('.total-pages').val(); x += hasil_bagi){
-		list_halaman.push(x)
-	}
-	if (list_halaman[list_halaman.length - 1] != $('.total-pages').val()){
-		list_halaman.push($('.total-pages').val())
-	}
-	$('.isi-target').html('')
-	for (x in list_halaman){
-		$('.isi-target').append('<tr><td class="cek-container"><div class="btn btn-default cek">&check;</div></td><td class="isi">Page '+list_halaman[x]+'</td></tr>')
-	}
-	$('.progress').show()
-	update_persen()
-	localStorage.setItem('reading challenge', $('.isi-target').html())
-})
-
-$(document).on('click', '.cek', function(){
-	$(this).toggleClass('btn-default').toggleClass('btn-success').toggleClass('cek-oke')
-	$(this).parent().parent().find('.isi').toggleClass('selesai')
-	update_persen()
-	localStorage.setItem('reading challenge', $('.isi-target').html())
-})
+// autocomplete(document.getElementById("myInput"), countries);
+autocomplete(document.getElementById('nama'), nama)
+autocomplete(document.getElementById('genre'), genre)
---
+++ app/src/main/assets/app/list.js
@@ -0,0 +1,106 @@
+nama = [
+	'Agus Heri Widodo',
+	'Ake Aulia Fitriana',
+	'Alfi Bahaviani',
+	'Alif Kiky Listiyati',
+	'Amie Oktiningsih Pamungkas',
+	'Andin Rahma Yulita',
+	'Arin Gudesma',
+	'Betty Irwanti, S.Pd.Sd',
+	'Danang Saputra',
+	'Devi Aulia Utami',
+	'Dian Fajar Rahmahyani Aprilia',
+	'Dilia Agustina',
+	'Dini Fauziah Gufron',
+	'Dita Dyah Saraswati',
+	'Dunyati Ilmiah',
+	'Dwi Budidarma',
+	'Dymar Mahafa',
+	'Elin Dwi Sulistyowati',
+	'Estina',
+	'Ezza Tania',
+	'Fatkhiatur Rohmah',
+	'Fatma Nofriyanti',
+	'Florensia Prihandini',
+	'Herdiawan Dwi Septiyana',
+	'Heru Widayanto',
+	'Hiday Nur',
+	'Ika Yudaningrum',
+	'Ilham Rahmanto',
+	'Irma Anggraini Adi',
+	'Irma Sari',
+	'Ismaniar',
+	'Isnaini Annisa',
+	'Juni Dwi Riyanti',
+	'Lia Anelia Nuraini',
+	'Lia Atis Sasmita',
+	'M. Saepudin',
+	'Moch Akbar Maulana',
+	'MS Wijaya',
+	'Muslikhotin',
+	'Muthya Sadeea',
+	'NA Fadli',
+	'Nimas Kesumo Zuhdiyah',
+	'Nining Purwanti',
+	'Nova DW',
+	'Novia Anggraini',
+	'Nur Ishaq',
+	'Nur Musabikah',
+	'Nurul Hidayah',
+	'Opsi 58',
+	'Pebri Ika Ndani',
+	'Rahayu',
+	'Raisha Nafisa',
+	'Rindang Yuliani',
+	'Riovani Dian Putri',
+	'Rofikoh Yuliyanti',
+	'Sakifah',
+	'Setya Romana',
+	'Shinta Dewi Wijiarti',
+	'Silvana Devinta Sari',
+	'Siti Maesaroh',
+	'Siti Rokhanah',
+	'Sri Lestari / El-Lisa',
+	'Suparto',
+	'Ulfi',
+	'Vinny Martina',
+	'Wakhid Syamsudin',
+	'Walimin',
+	'Widhyanua Sarjana Aryandari',
+	'Winarto Sabdo',
+	'Winda',
+	'Wiwid NW',
+	'Wiwin Asriningrum',
+	'Yeti Nuryeti',
+	'Yogi Riyansyah Suaji',
+	'Yolanda Triana Putri',
+	'Yuliani'
+]
+
+genre = [
+	'Fiksi',
+	'NonFiksi'
+]
+
+quote = [
+	[
+		'Badan bisa mati, tulisanmu abadi.',
+		'Hiday Nur'
+	],
+	[
+		'Tulisanku takkan pernah mengkhianatiku.',
+		'Zen'
+	],
+	[
+		'Ayo semangat, 1 bab lagi bukumu akan selesai.',
+		'Tanpa Nama'
+	],
+	[
+		'Dengan membaca aku melihat dunia, dengan menulis aku berpesan untuk dunia',
+		'Winda'
+	],
+	[
+		'Penciptaan karya (apapun itu) bukan perkara sebagai pembanding antara siapa yang baik, siapa yang buruk, tetapi soal sejauh mana manusia mampu mencapai versi terbaik dari dirinya, untuk kemudian mengabadikan itu (versi terbaiknya) ke dalam bentuk yang mampu memberi katarsis (pembaruan rohani); termasuk menulis.',
+		'Dymar Mahafa'
+	]
+]
